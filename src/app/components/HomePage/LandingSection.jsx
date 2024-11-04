0af68a9f52377392b7ee10c17eb7a09467db9fbe
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2024-11-04 12:12:24 -0300

    [New]: Logo PWA, Text landing section.

--- src/app/components/HomePage/LandingSection.jsx
+++ src/app/components/HomePage/LandingSection.jsx
@@ -1,16 +1,15 @@
 import Image from "next/image";
-export const LandingSection= ({ ImgUrl }) => {
+export const LandingSection = ({ ImgUrl }) => {
     return (
         <div className="container">
             <div className="row d-flex align-items-center justify-content-center">
                 <div className="col-md-6 px-1 lh-lg text-wrap">
-                    <Image className="img-fluid" src='/Images/Titulo.png' width={800} height={800} alt='Imagen Principal' priority/>
+                    <Image className="img-fluid" src='/Images/Titulo.png' width={800} height={800} alt='Imagen Principal' priority />
                     <p className="text-center">
-                        Nuestra plataforma e-learning tiene un gran propósito, ofrecer cursos, talleres y diplomados relacionados
-                        con las necesidades educativas de la región,  para potenciar el aprendizaje en contextos formales y no formales.
-                        Esta herramienta educativa virtual se puede usar en computadoras y dispositivos móviles como teléfonos y tabletas,
-                        lo que brinda a los usuarios acceso al contenido educativo en cualquier momento y lugar.
+                        Nuestra plataforma e-learning tiene un gran propósito, ofrecer cursos, talleres y diplomados relacionados con las necesidades educativas de la región, para potenciar el aprendizaje en contextos formales y no formales.
                     </p>
+                    <p className="text-center">
+                        Esta herramienta educativa virtual se puede usar en computadoras y dispositivos móviles como teléfonos y tabletas, lo que brinda a los usuarios acceso al contenido educativo en cualquier momento y lugar.                    </p>
                 </div>
                 <div className="col-md-6 row mt-2">
                     <Image className="img-fluid" src={ImgUrl} width={800} height={800} alt='Imagen Principal' />
--- src/app/components/Modulos/componentsSecciones/AlertFinishSection/Alert.jsx
+++ src/app/components/Modulos/componentsSecciones/AlertFinishSection/Alert.jsx
@@ -13,8 +13,8 @@ export const Alert = ({ seccionNumber }) => {
                 <div className="LevelCompleted" >
                     <div className='alert alert-light' role='alert'>
                         <button type="button" className="btn-close float-end" data-bs-dismiss="alert" aria-label="Close" onClick={() => setFinished(false)}></button>
-                        <h4 className="alert-heading mb-3">{`Sectión ${seccionNumber} Completada!`}</h4>
-                        <img src="/Images/Donaciones.png" alt="Imagen de nivel completado" width={200} height={200} />
+                        <h4 className="alert-heading mb-3">{`Sección ${seccionNumber} Completada!`}</h4>
+                        <img src="/Images/Home.png" alt="Imagen de nivel completado" width={200} height={200} />
                         <hr />
                         <Link href={`/${cursoName}/${cursoLevel}/${seccionNumber + 1}`} className="btn btn-primary" onClick={() => setFinished(false)}>
                             Comienza el siguiente nivel aquí
