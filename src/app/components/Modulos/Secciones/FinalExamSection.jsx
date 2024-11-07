5fb4f304dae4eb06114c06bed1e4bc53b1a6797d
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2024-11-07 08:50:18 -0300

    Update FinalExamSection.jsx

--- src/app/components/Modulos/Secciones/FinalExamSection.jsx
+++ src/app/components/Modulos/Secciones/FinalExamSection.jsx
@@ -1,13 +1,25 @@
 import { getSession } from '@auth0/nextjs-auth0';
-import Certificado from "../../Certificado"
+import { FilePdf, Activities, ContenidoTeorico } from "../componentsSecciones";
+// import Certificado from "../../Certificado"
+
 export const FinalExamSection = async () => {
     const { user } = await getSession();
 
     return (
-        <div className="final-exam-section">
-            <h1>Final Exam Section</h1>
-            <p>{user.name}</p>
-            <Certificado nombre={user.name}/>
+        <div className="container">
+            <div className="row d-flex align-items-center justify-content-center mt-4">
+                <div className="col-md-6 text-center">
+                    <Activities />
+                </div>
+                <div className="col-md-6">
+                    <ContenidoTeorico maxHeight={"640px"} />
+                    {/* <Certificado nombre={user.name} /> */}
+                </div>
+            </div>
         </div>
+        // <div className="final-exam-section">
+        //     <h1>Final Exam Section</h1>
+        //     <p>{user.name}</p>
+        // </div>
     )
 }
