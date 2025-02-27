30574d78a0bbbc188a63df0790823c30375959fc
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2025-02-27 13:18:01 -0300

    [New]: layout Modulo-1 Tica

---
+++ src/app/Tica/Modulo-1/[number]/layout.jsx
@@ -0,0 +1,26 @@
+'use client'
+import { DataActivitiesContext } from "@/app/contexts/DataActivities-context";
+import { useContext, useEffect } from "react"
+import { useDataFromSheets } from "@/app/hooks/useDataFromSheets";
+
+export default function RootLayout({ children }) {
+    const { setData } = useContext(DataActivitiesContext);
+
+    const { data: preguntas, isLoading, error } = useDataFromSheets("https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5lG7ihT8C5idJRf9PFkm9tzR8LBhKlak8Z0prnaBGNso63Hd9VstJCldz7NDJvbiaM0AP3IKY1-22/pub?output=csv");
+
+    useEffect(() => {
+        if (!isLoading && !error) {
+            setData(preguntas);
+        }
+    }, [preguntas, isLoading, error, setData]);
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+
+    return (
+        <>
+            {children}
+        </>
+    );
+}
