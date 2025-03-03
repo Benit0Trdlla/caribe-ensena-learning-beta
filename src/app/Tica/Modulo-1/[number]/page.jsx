3b467c476360859fcfc39c939ef6b2b816d6dbd4
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2025-03-03 17:03:37 -0300

    [New]: page Modulo-1 'Tica' course.

---
+++ src/app/Tica/Modulo-1/[number]/page.jsx
@@ -0,0 +1,36 @@
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
+import {
+    BookSection,
+    VideoSection,
+    FinalExamSection,
+    Header,
+    Buttons,
+    SectionNavigation,
+    Alert
+} from '../../../components/Modulos';
+
+export function generateMetadata({ params }) {
+    return {
+        title: `Modulo 1 | Seccion ${params.number}`,
+    };
+}
+
+function TicaModuloPage({ params }) {
+    const number = params.number
+    const numInt = parseInt(number)
+    const ComponentsSections = [<BookSection />, <BookSection />, <VideoSection />, <BookSection />, <VideoSection />, <VideoSection />, <FinalExamSection />]
+
+    return (
+        <>
+            {/* <Header ImgUrl={"/Images/Logo_Cursos/NewSkills.png"} href="/Cursos" indexMeet={1} /> */}
+            <Alert seccionNumber={numInt} />
+            <SectionNavigation />
+            {ComponentsSections[numInt - 1]}
+            {/* <Buttons NumInt={numInt} CursoPath={"/Newskills/Modulo-1/"} /> */}
+        </>
+    )
+}
+
+export default withPageAuthRequired(TicaModuloPage, {
+    returnTo: ({ params }) => `/Tica/Modulo-1/${params.number}`,
+});
