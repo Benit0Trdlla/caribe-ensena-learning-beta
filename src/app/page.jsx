4672778ec2a884615c626ba9ded83f746476d3ff
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2025-05-21 09:38:44 -0300

    Test: Loading Environment Variables

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,9 +9,16 @@ import {
   AliadosSection
  } from "./components/HomePage";
 export default function Home() {
+  
+  const testURL = process.env.AUTH0_BASE_URL
+  const testISSUERURL = process.env.AUTH0_ISSUER_BASE_URL
+  
+
   return (
     <main>
       <Header imgUrl={"/Images/Logo/Header.png"} />
+      {testURL}
+      {testISSUERURL}
       <LandingSection ImgUrl="/Images/Home.png" />
       <ServicioComponente />
       <PreguntasFrecuentesComponente />
