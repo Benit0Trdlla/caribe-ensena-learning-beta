bdd7ab6014ef58399cb49b804822fdc1b3a461d6
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2025-07-03 10:46:42 -0300

    change. 'audio' tag to 'iframe' tag in the component 'Audios'.

--- src/app/components/Modulos/componentsSecciones/AddImagen/AddImagen.jsx
+++ src/app/components/Modulos/componentsSecciones/AddImagen/AddImagen.jsx
@@ -3,12 +3,12 @@ export const AddImagen = ({ alt, dataUrl }) => {
         <div className="row container">
             {dataUrl.split(',').map((imagen, index) => (
                 <div className="col-6" key={index}>
-                    <button type="button" class="bg-transparent border-0 px-0 my-2" data-bs-toggle="modal" data-bs-target="#ModalImagen">
+                    <button type="button" className="bg-transparent border-0 px-0 my-2" data-bs-toggle="modal" data-bs-target="#ModalImagen">
                         <img src={imagen.trim()} alt={alt} className="img-fluid" />
                     </button>
 
                     {/* <!-- Modal --> */}
-                    <div className="modal fade" id="ModalImagen" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal fade" id="ModalImagen" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                         <div className="modal-dialog modal-dialog-centered">
                             <div className="modal-content">
                                 <div className="modal-header">
--- src/app/components/Modulos/componentsSecciones/Audios/Audios.jsx
+++ src/app/components/Modulos/componentsSecciones/Audios/Audios.jsx
@@ -1,10 +1,12 @@
+import "./style.css";
 export const Audios = ({ url }) => {
     return (
         <div className='d-flex justify-content-center mt-3'>
-            <audio controls>
+            {/* <audio controls>
                 <source src={url} type="audio/aac" />
                 Tu navegador no soporta el audio.
-            </audio>
+            </audio> */}
+            <iframe className="dimensions dimensions-xs" src={url} title="Audio de ayuda"></iframe>
         </div>
     );
 };
