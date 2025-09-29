ec3cc33250963e2213b18c7013c6b5aac90c823f
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2025-09-29 11:59:10 -0300

    Add. Display multiples audios.

--- src/app/components/Modulos/componentsSecciones/Audios/Audios.jsx
+++ src/app/components/Modulos/componentsSecciones/Audios/Audios.jsx
@@ -1,12 +1,14 @@
 import "./style.css";
 export const Audios = ({ url }) => {
     return (
-        <div className='d-flex justify-content-center mt-3'>
+        <div className='text-center mt-3'>
+            {url.split(',').map((audio, index) => (
+                <iframe key={index} className="dimensions dimensions-xs" src={audio} title="Audio de ayuda"></iframe>
+            ))}
             {/* <audio controls>
                 <source src={url} type="audio/aac" />
                 Tu navegador no soporta el audio.
             </audio> */}
-            <iframe className="dimensions dimensions-xs" src={url} title="Audio de ayuda"></iframe>
         </div>
     );
 };
