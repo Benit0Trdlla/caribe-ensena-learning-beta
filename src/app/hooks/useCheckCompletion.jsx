83d40975e5f5dee83edafdeab7f4c5f64ce5df84
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2025-10-28 10:43:11 -0300

    Update useCheckCompletion.jsx

--- src/app/hooks/useCheckCompletion.jsx
+++ src/app/hooks/useCheckCompletion.jsx
@@ -1,17 +1,17 @@
 import { useMemo } from "react";
 
-export function useCheckCompletion({ modulo = 100, seccionCompleted, numInt }) {
+export function useCheckCompletion({ modulo = null, seccionCompleted, numInt }) {
     const CheckCompletion = useMemo(() => {
         if (modulo !== null && modulo < 100) {
             return {
-                message: `El Modulo anterior no fue completado TEST`,
+                message: `El Modulo anterior no fue completado`,
                 isCompleted: false
             };
         }
 
         if (!seccionCompleted && numInt !== 1) {
             return {
-                message: `La Seccion ${numInt - 1} no fue completada EST`,
+                message: `La Seccion ${numInt - 1} no fue completada`,
                 isCompleted: false
             };
         }
