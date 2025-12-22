d58683a86aaab6f0a0c5c025e77813ce3793e0c5
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2025-12-22 09:17:31 -0300

    chore: condiitona href with user session.

--- src/app/components/ExplicacionCursos/Header.jsx
+++ src/app/components/ExplicacionCursos/Header.jsx
@@ -3,12 +3,17 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { Point } from '../Modulos'
 import { useLinkMeet } from '../../hooks/useLinkMeet'
-
+import { useUser } from "@auth0/nextjs-auth0/client";
 const URL_SHEETS = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSWEDU8Ty2Bz_cMSvvm-YxRWXima4w5dD1VoLwAZYTOZ756z3RQkAwphrX6_F74BWnWN9VJbUvgz7_0/pub?output=csv"
 export const Header = ({ ImgUrl, href, indexMeet }) => {
     const IndexMeetData = indexMeet;
+    const { user } = useUser();
+
     const { data, isLoading, error } = useLinkMeet(URL_SHEETS)
     if (error) return <div>{error.message}</div>
+
+    const conditionalHref = user ? `/Cursos` : `/`;
+
     return (
         <>
             <header id="header">
@@ -17,7 +22,7 @@ export const Header = ({ ImgUrl, href, indexMeet }) => {
                         <Image src={ImgUrl} width={60} height={60} priority={true} className="navbar-brand img-fluid" alt="Logo Proyecto Genius" style={{ width: "auto" }} />
                         <div className='d-flex justify-content-center'>
                             {!isLoading &&
-                                <a href={`https://${data[IndexMeetData].URL}`} title={data[IndexMeetData].Title} className='btn border me-4 ms-lg-0 d-flex align-items-center gap-2' target="_blank" type='button'>
+                                <a href={`https://${data[indexMeet].URL}`} title={data[indexMeet].Title} className='btn border me-4 ms-lg-0 d-flex align-items-center gap-2' target="_blank" type='button'>
                                     <Point />
                                     ¡Únete al evento!
                                 </a>
@@ -31,7 +36,7 @@ export const Header = ({ ImgUrl, href, indexMeet }) => {
                                 <Link prefetch={true} href={`${href}/Progreso`} className="btn btn-info" style={{ backgroundColor: "#beefff", borderColor: "#beefff" }}>
                                     <small>Mi Progreso</small>
                                 </Link>
-                                <Link href='/Cursos' className="btn btn-info" style={{ backgroundColor: "#beefff", borderColor: "#beefff" }}>
+                                <Link href={conditionalHref} className="btn btn-info" style={{ backgroundColor: "#beefff", borderColor: "#beefff" }}>
                                     <small>Volver</small>
                                 </Link>
                             </ul>
