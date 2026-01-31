c48b155df9efed9735c97086dd3996dfdbf18d4e
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2026-01-31 12:54:55 -0300

    test: Tica background image.

--- src/app/Tica/layout.jsx
+++ src/app/Tica/layout.jsx
@@ -2,13 +2,17 @@ import DataActivitiesContextProvider from "@/app/contexts/DataActivities-context
 import FinishSectionContextProvider from "../contexts/FinishSection-context";
 import { Header } from "../components/ExplicacionCursos";
 
+// Fondo de la página para la ruta /Tica
+// Se aplica un contenedor con fondo responsive a todo el layout
 export default function RootLayout({ children }) {
-    return (
-        <DataActivitiesContextProvider>
-            <FinishSectionContextProvider>
-                <Header ImgUrl={"/Images/Logo_Cursos/Tica.png"} href={"/Tica"} indexMeet={2} />
-                {children}
-            </FinishSectionContextProvider>
-        </DataActivitiesContextProvider>
-    );
+  return (
+    <div className="tica-background-root">
+      <DataActivitiesContextProvider>
+        <FinishSectionContextProvider>
+          <Header ImgUrl={"/Images/Logo_Cursos/Tica.png"} href={"/Tica"} indexMeet={2} />
+          {children}
+        </FinishSectionContextProvider>
+      </DataActivitiesContextProvider>
+    </div>
+  );
 }
--- src/app/Tica2/layout.jsx
+++ src/app/Tica2/layout.jsx
@@ -4,11 +4,13 @@ import { Header } from "../components/ExplicacionCursos";
 
 export default function RootLayout({ children }) {
     return (
-        <DataActivitiesContextProvider>
-            <FinishSectionContextProvider>
-                <Header ImgUrl={"/Images/Logo_Cursos/Tica2.png"} href={"/Tica2"} indexMeet={3} />
-                {children}
-            </FinishSectionContextProvider>
-        </DataActivitiesContextProvider>
+        <div className="tica-background-root">
+            <DataActivitiesContextProvider>
+                <FinishSectionContextProvider>
+                    <Header ImgUrl={"/Images/Logo_Cursos/Tica2.png"} href={"/Tica2"} indexMeet={3} />
+                    {children}
+                </FinishSectionContextProvider>
+            </DataActivitiesContextProvider>
+        </div>
     );
 }
