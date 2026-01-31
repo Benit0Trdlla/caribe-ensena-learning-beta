716e48c787908ba51b99ed2cdde62a4129a7350a
Benito Tridella Dolce <benitotridelladolce@gmail.com>
2026-01-31 12:02:57 -0300

    chore: removal of title from events button.

--- src/app/components/ExplicacionCursos/Header.jsx
+++ src/app/components/ExplicacionCursos/Header.jsx
@@ -23,7 +23,7 @@ export const Header = ({ ImgUrl, href, indexMeet }) => {
                             {!isLoading &&
                                 <a href={`https://${data[indexMeet].URL}`} title={data[indexMeet].Title} className='btn border me-4 ms-lg-0 d-flex align-items-center gap-2' target="_blank" type='button'>
                                     <Point />
-                                    ¡Únete al evento!
+                                    {/* ¡Únete al evento! */}
                                 </a>
                             }
                         </div>
